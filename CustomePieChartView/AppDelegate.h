35ed0095e04f54e03d38f3fcccb85a3f62c4fa38
chengyiwan <1017866288@qq.com>
2016-09-28 09:20:15 +0800

    Initial Commit

---
+++ CustomePieChartView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomePieChartView
+//
+//  Created by apple-CXTX on 16/9/28.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomePieChartView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomePieChartView
+//
+//  Created by apple-CXTX on 16/9/28.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
